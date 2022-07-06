5df1df1cb157aa42edb19e8de6e93dbe8a5dbd82
rusbel <santosvanegas13@gmail.com>
2022-07-06 00:46:39 -0500

    Taller javascript
    formulario sin validacion

---
+++ JS - Tail/src/scripts/myscript.js
@@ -0,0 +1,40 @@
+
+const formulario = document.getElementById('formulario');
+
+
+
+nombre.addEventListener('keydown',(e)=>{
+    if(esalphanum(e.target.value)){
+        
+        errorNombre.classList.remove('hidden')
+    }
+    else{
+        
+        errorNombre.classList.add('hidden')
+    }
+ 
+})
+
+codigo.addEventListener('keydown',(e)=>{
+    if(esalphanum(e.target.value)){
+        
+        errorId.classList.remove('hidden')
+    }
+    else{
+        
+        errorId.classList.add('hidden')
+    }
+ 
+})
+
+addEventListener('submit', ()=>{ 
+    
+});
+
+function esalphanum (value){
+    if (value.match(/^[0-9a-zA-Z]+$/)){
+        return false;
+    }else{
+        return true;
+    }
+}
